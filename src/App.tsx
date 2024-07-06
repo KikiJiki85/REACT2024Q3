c8ed911bab7eb48c53f8a6b57fcaa0535baa6ce3
Aleksey Vaskin <alzor85@gmail.com>
2024-07-06 13:31:19 +0300

    feat: begin loader implementation

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ interface ApiResponse {
 interface AppState {
     searchTerm: string;
     results: { name: string; description: string }[];
+    isLoading: boolean;
 }
 
 class App extends Component<{}, AppState> {
@@ -22,69 +23,76 @@ class App extends Component<{}, AppState> {
         this.state = {
             searchTerm: "",
             results: [],
+            isLoading: false,
         };
     }
 
     componentDidMount() {
         const savedSearchTerm = localStorage.getItem("searchTerm");
+
         if (savedSearchTerm) {
-            this.setState({ searchTerm: savedSearchTerm }, this.fetchResults);
+            this.setState({ searchTerm: savedSearchTerm }, () => {
+                this.fetchResults(savedSearchTerm);
+            });
         } else {
-            this.fetchResults();
+            this.fetchResults("");
         }
     }
 
-    fetchResults = () => {
-        const { searchTerm } = this.state;
+    fetchAllCharacters = () => {
+        this.setState({ isLoading: true });
+        fetch("https://stapi.co/api/v1/rest/character/search", {
+            method: "GET",
+        })
+            .then(response => response.json())
+            .then((data: ApiResponse) => {
+                const results = data.characters.map((character: Character) => ({
+                    name: character.name,
+                    description: character.uid,
+                }));
+                this.setState({ results, isLoading: false });
+            })
+            .catch(error => console.error("Error fetching data:", error));
+    };
+
+    fetchCharactersByName = (name: string) => {
+        const formData = new URLSearchParams();
+        formData.append("name", name);
+
+        fetch(`https://stapi.co/api/v1/rest/character/search/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: formData.toString(),
+        })
+            .then(response => response.json())
+            .then((data: ApiResponse) => {
+                const results = data.characters.map((character: Character) => ({
+                    name: character.name,
+                    description: character.uid,
+                }));
+                this.setState({ results, isLoading: false });
+            })
+            .catch(error => console.error("Error fetching data:", error));
+    };
+
+    fetchResults = (searchTerm: string) => {
         const trimmedSearchTerm = searchTerm.trim();
 
         if (trimmedSearchTerm) {
-            const formData = new URLSearchParams();
-            formData.append("name", trimmedSearchTerm);
-
-            fetch(`https://stapi.co/api/v1/rest/character/search/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                },
-                body: formData.toString(),
-            })
-                .then(response => response.json())
-                .then((data: ApiResponse) => {
-                    const results = data.characters.map(
-                        (character: Character) => ({
-                            name: character.name,
-                            description: character.uid,
-                        }),
-                    );
-                    this.setState({ results });
-                })
-                .catch(error => console.error("Error fetching data:", error));
+            this.fetchCharactersByName(trimmedSearchTerm);
         } else {
-            fetch("https://stapi.co/api/v1/rest/character/search", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-                .then(response => response.json())
-                .then((data: ApiResponse) => {
-                    const results = data.characters.map(
-                        (character: Character) => ({
-                            name: character.name,
-                            description: character.uid,
-                        }),
-                    );
-                    this.setState({ results });
-                })
-                .catch(error => console.error("Error fetching data:", error));
+            this.fetchAllCharacters();
         }
     };
 
     handleSearch = (term: string) => {
         const trimmedTerm = term.trim();
         localStorage.setItem("searchTerm", trimmedTerm);
-        this.setState({ searchTerm: trimmedTerm }, this.fetchResults);
+        this.setState({ searchTerm: trimmedTerm }, () => {
+            this.fetchResults(trimmedTerm);
+        });
     };
 
     render() {
