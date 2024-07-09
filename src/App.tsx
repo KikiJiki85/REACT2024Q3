ba1858030372d6e220769123f416d448d97c8d70
Aleksey Vaskin <alzor85@gmail.com>
2024-07-09 17:10:01 +0300

    feat: rewrite App component to functional with custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,56 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import SearchBar from './components/SearchBar';
 import SearchResults from './components/SearchResult';
 import ErrorBoundary from './components/ErrorBoundary';
 import { fetchResults } from './api/api';
+import useSearchTerm from './useSearchTerm';
 
-interface AppState {
-  searchTerm: string;
-  results: { name: string; description: string }[];
-  isLoading: boolean;
-}
+const App: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useSearchTerm('');
+  const [results, setResults] = useState<
+    { name: string; description: string }[]
+  >([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-      results: [],
-      isLoading: false,
-    };
-  }
+  useEffect(() => {
+    fetchResults(searchTerm, updateState);
+  }, [searchTerm]);
 
-  componentDidMount() {
-    const savedSearchTerm = localStorage.getItem('searchTerm');
-
-    if (savedSearchTerm) {
-      this.setState({ searchTerm: savedSearchTerm }, () => {
-        fetchResults(savedSearchTerm, this.updateState);
-      });
-    } else {
-      fetchResults('', this.updateState);
-    }
-  }
-
-  handleSearch = (term: string) => {
+  const handleSearch = (term: string) => {
     const trimmedTerm = term.trim();
-    localStorage.setItem('searchTerm', trimmedTerm);
-    this.setState({ searchTerm: trimmedTerm }, () => {
-      fetchResults(trimmedTerm, this.updateState);
-    });
+    setSearchTerm(trimmedTerm);
+    setIsLoading(true);
+    fetchResults(trimmedTerm, updateState);
   };
 
-  updateState = (state: Partial<AppState>) => {
-    this.setState(state as Pick<AppState, keyof AppState>);
+  const updateState = (
+    state: Partial<{
+      results: { name: string; description: string }[];
+      isLoading: boolean;
+    }>,
+  ) => {
+    if (state.results) setResults(state.results);
+    if (state.isLoading !== undefined) setIsLoading(state.isLoading);
   };
 
-  render() {
-    const { searchTerm, results, isLoading } = this.state;
-
-    return (
-      <ErrorBoundary>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100vh',
-          }}
-        >
-          <div style={{ flex: '1', borderBottom: '1px solid black' }}>
-            <SearchBar searchTerm={searchTerm} onSearch={this.handleSearch} />
-          </div>
-          <div style={{ flex: '4', overflowY: 'scroll' }}>
-            <SearchResults results={results} isLoading={isLoading} />
-          </div>
+  return (
+    <ErrorBoundary>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100vh',
+        }}
+      >
+        <div style={{ flex: '1', borderBottom: '1px solid black' }}>
+          <SearchBar searchTerm={searchTerm} onSearch={handleSearch} />
         </div>
-      </ErrorBoundary>
-    );
-  }
-}
+        <div style={{ flex: '4', overflowY: 'scroll' }}>
+          <SearchResults results={results} isLoading={isLoading} />
+        </div>
+      </div>
+    </ErrorBoundary>
+  );
+};
 
 export default App;
