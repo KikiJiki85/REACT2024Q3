9cc05182f9f9b8a7d499ac5edd1be9c9095b9bef
Aleksey Vaskin <alzor85@gmail.com>
2024-07-10 14:25:01 +0300

    feat: add AppRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import AppRouter from './AppRouter.tsx';
 
 const element = document.getElementById('root')!;
 const root = ReactDOM.createRoot(element);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
 );
